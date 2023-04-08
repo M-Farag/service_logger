a0a6c7ff694f193c746d12e089feffb604e49169
M-Farag <mina.farag@icloud.com>
2023-04-08 23:27:21 +0200

    UPD Add the logging functionality and link it to the main file

--- src/main.rs
+++ src/main.rs
@@ -2,10 +2,26 @@ use service_logger::{self, Config};
 
 fn main() {
     println!("Service is running...");
+
     let args = Config::new().unwrap_or_else(
         |err| {
             eprintln!("Err: {}", err);
             std::process::exit(1);
         }
-    );    
+    );   
+
+    let mut log_file = service_logger::get_an_appending_file_handler(&args.file_path).unwrap_or_else(
+        |err| {
+            eprintln!("Err: {}", err);
+            std::process::exit(1);
+        }
+    );
+
+
+    if let Err(err) =  service_logger::write_to_file(&mut log_file, &args.message) {
+        eprintln!("Err: {}", err);
+        std::process::exit(1);
+    }
+    
+    println!("Service is done.");
 }
